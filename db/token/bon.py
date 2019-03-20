91049ccb9dc7bd8dbf95a5ae4410c19d41fdcce5
Ivan Pillay <Ivan.Pillay@harbingergroup.com>
2019-03-20 16:29:01 +0530

    geth nodes for app & token

---
+++ db/token/bon.py
@@ -0,0 +1,51 @@
+"""
+Geth wrapper
+"""
+
+import subprocess
+import json
+import sys
+
+
+def initGeth():
+    with open('config.json') as file:
+        args = json.load(file)
+        subprocess.call("geth init "
+                        "--datadir {} "
+                        " {}".format(args["data_dir"], args["genesis"]))
+
+
+def mineGeth():
+    with open('config.json') as file:
+        args = json.load(file)
+        subprocess.call("geth --networkid {} "
+         "--datadir {} "
+         "--rpc --rpcapi personal,eth,rpc,web3,net "
+         "--rpcaddr {} --rpcport {} --mine"
+         .format(args["net_id"], args["data_dir"], args["rpc_addr"], args["rpc_port"]))
+
+
+def attachGethHttp():
+    with open('config.json') as file:
+        args = json.load(file)
+        provider = "http://{}:{}".format(args["rpc_addr"],args["rpc_port"])
+        print('Connecting to: '+provider);
+        subprocess.call("geth attach {} ".format(provider))
+
+
+def help():
+    print('USAGE: <script> init | mine | attach\n')
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == 'init'):
+            initGeth()
+        elif(sys.argv[1] == 'mine'):
+            mineGeth()
+        elif(sys.argv[1] == 'attach'):
+            attachGethHttp()
+        else:
+            help()
+    else:
+        help()
